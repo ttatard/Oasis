f6a8254c13c5d07a9f5271b57558d95c8e8e99fc
ttatard <richard.molina@cit.edu>
2023-05-19 04:55:37 +0800

    hello

--- app/src/main/java/com/example/oasis/Chat.java
+++ app/src/main/java/com/example/oasis/Chat.java
@@ -2,10 +2,11 @@ package com.example.oasis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.content.Intent;
+
 
 public class Chat extends AppCompatActivity {
 
@@ -13,27 +14,27 @@ public class Chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        ImageView imageView3 = findViewById(R.id.imageView3);
+        imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click event here
+                // Example: Redirect to another activity
+                Intent intent = new Intent(Chat.this, Bob.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView imageView4 = findViewById(R.id.imageView4);
+        imageView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click event here
+                // Example: Redirect to another activity
+                Intent intent = new Intent(Chat.this, Edna.class);
+                startActivity(intent);
+            }
+        });
     }
-    ImageView imageView3 = findViewById(R.id.imageView3);
-imageView3.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            // Handle the click event here
-            // Example: Redirect to another activity
-            Intent intent = new Intent(MainActivity.this, AnotherActivity.class);
-            startActivity(intent);
-        }
-    });
-
-    ImageView imageView4 = findViewById(R.id.imageView4);
-imageView4.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            // Handle the click event here
-            // Example: Redirect to another activity
-            Intent intent = new Intent(MainActivity.this, AnotherActivity.class);
-            startActivity(intent);
-        }
-    });
-
 }
--- app/src/main/java/com/example/oasis/MainActivity.java
+++ app/src/main/java/com/example/oasis/MainActivity.java
@@ -2,13 +2,10 @@ package com.example.oasis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.oasis.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private Button createBtn;
@@ -45,20 +42,18 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     public void openForgotPasswordPage(){
-        Intent i = new Intent(this, forgotPassword.class);
+        Intent i = new Intent(MainActivity.this, forgotPassword.class);
         startActivity(i);
     }
 
     public void openCreateAccountPage(){
-        Intent i = new Intent(this, createAccount.class);
+        Intent i = new Intent(MainActivity.this, createAccount.class);
         startActivity(i);
     }
 
     public void homePage(){
-        Intent i = new Intent(this, homePage.class);
+        Intent i = new Intent(MainActivity.this, homePage.class);
         startActivity(i);
     }
-
 }
